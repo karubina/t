565d23d4ac1e3713bbfcc155c7d14ba54ddce5b2
karubina <karpova.ekaterina19@gmail.com>
2022-01-19 09:07:24 +0300

    Initial Commit

---
+++ t/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  t
+//
+//  Created by Екатерина Рубина on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
